54744b7ee9beca96fd503c5d0ba32a6739c60074
Phuc Huynh <huynhhuuphuc100599@gmail.com>
2021-06-18 19:05:55 +0700

    add login page / sign with Google / add data in firestore

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./styles.scss";
 import { Link } from "react-router-dom";
+import { auth } from "./../../firebase/utils";
 
 const Header = (props) => {
+  const { currentUser } = props;
   return (
     <header className="header">
       <div className="wrap">
@@ -13,15 +15,32 @@ const Header = (props) => {
         </div>
 
         <div className="callToActions">
-          <ul>
-            <li>
-              <Link to="/registration">Register</Link>
-            </li>
-          </ul>
+          {currentUser && (
+            <ul>
+              <li>
+                <span onClick={() => auth.signOut()}>LogOut</span>
+              </li>
+            </ul>
+          )}
+
+          {!currentUser && (
+            <ul>
+              <li>
+                <Link to="/registration">Register</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          )}
         </div>
       </div>
     </header>
   );
+};
+
+Header.defaultProps = {
+  currentUser: null,
 };
 
 export default Header;
---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCweSgzEaV_zZH_l1X-4elsxqzi94lhV50",
+  authDomain: "ecommerce-website-8686e.firebaseapp.com",
+  projectId: "ecommerce-website-8686e",
+  storageBucket: "ecommerce-website-8686e.appspot.com",
+  messagingSenderId: "551792623355",
+  appId: "1:551792623355:web:bd496f20cbe179f6d0ba5e",
+  measurementId: "G-SSSKB66ZK2",
+};
