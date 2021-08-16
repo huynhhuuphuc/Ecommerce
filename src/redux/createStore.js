16b8b6b8305f149622d6fdc504ef94cf8847da51
Phuc Huynh <huynhhuuphuc100599@gmail.com>
2021-08-16 09:05:08 +0700

    redux persist

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -1,8 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
-
 import thunk from "redux-thunk";
 import createSagaMiddleware from "redux-saga";
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./rootReducer";
 import rootSaga from "./rootSaga";
@@ -14,4 +14,6 @@ export const middlewares = [thunk, sagaMiddleware, logger];
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 sagaMiddleware.run(rootSaga);
 
-export default store;
+export const persistor = persistStore(store); // luu state vao local storange
+
+export default { store, persistor };
